2a52d3f3b301be0a8c91fe1e78e2fbb9360f44bb
Lokesh <lokeshloke0502@gmail.com>
2026-08-13 19:35:32 +0530

    derived journey duration in minutes

--- level3.py
+++ level3.py
@@ -14,3 +14,38 @@ df["departure_time"] = pd.to_datetime(
 
 print(df[["arrival_time", "departure_time"]].dtypes)
 
+# 3.2 Derive a new feature representing journey duration
+
+df = df.sort_values(
+    ["train_number", "route_number"]
+).reset_index(drop=True)
+
+# To find the next stations arrival time
+
+df["next_arrival_time"] = (
+    df.groupby("train_number")["arrival_time"]
+      .shift(-1)
+)
+
+# Then calculating the duration 
+
+df["journey_duration"] = (
+    df["next_arrival_time"] - df["departure_time"]
+)
+
+# Handle midnight crossing 
+
+df.loc[
+    df["journey_duration"] < pd.Timedelta(0),
+    "journey_duration"
+] += pd.Timedelta(days=1)
+
+
+# Convert it into minutes 
+
+df["journey_duration_minutes"] = (
+    df["journey_duration"]
+    .dt.total_seconds() / 60
+)
+
+df.head()
