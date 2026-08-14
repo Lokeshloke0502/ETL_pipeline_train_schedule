ad105e990ce1d02636cd8abfcfee854565c68fc1
Lokesh <lokeshloke0502@gmail.com>
2026-08-14 19:08:56 +0530

    cross table created

--- level4.py
+++ level4.py
@@ -4,14 +4,20 @@ df = pd.read_csv("Validate_dataset.csv")
 
 print(df.shape)
 
-# Generate a train-level table showing number of stops
+# 4.1 Generate a train-level table showing number of stops
 
 train_stops = (df.groupby("train_number").agg(number_of_stops = ("station_code","count")).reset_index())
 
 print(train_stops.head())
 
-# Generate a train-level table showing total distance
+# 4.2 Generate a train-level table showing total distance
 
-train_distance = (df.groupby("train_number").agg(total_distance_traveled = ("distance","max")).reset_index())
+train_distances = (df.groupby("train_number").agg(total_distance_traveled = ("distance","max")).reset_index())
 
-print(train_distance.head())
+print(train_distances.head())
+
+# 4.3: Create a cross table comparing trains and stations
+
+train_station_cross_table = pd.crosstab(df["train_number"],df["station_name"])
+
+print(train_station_cross_table.head())
