402de9de2e8b064e03f62474ba984c8488592d99
Lokesh <lokeshloke0502@gmail.com>
2026-08-19 18:55:45 +0530

    all necessary libraries are imported

---
+++ level6.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import (
+    mean_absolute_error,
+    mean_squared_error,
+    r2_score
+)
